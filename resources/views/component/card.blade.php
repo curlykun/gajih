85ddbbcdd3afec67266ed0ac7c21058b70fc0f87
kurob <kurob1993@gmail.com>
2018-10-10 23:24:51 +0700

    Merubah Header Jadi darak/hitam

--- resources/views/component/card.blade.php
+++ resources/views/component/card.blade.php
@@ -1,6 +1,6 @@
 <div class="animated fadeIn">
   <div class="card">
-    <div class="card-header">
+    <div class="card-header bg-dark">
       {{ $title }}
     </div>
     <div class="card-body">
